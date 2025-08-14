aa640becc625df6e366e595eac7a404576a78fe6
Yousef754 <yosefesam987@gmail.com>
2025-08-14 23:01:19 +0300

    part02

---
+++ Session3_adv/BookFunction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3_adv
+{
+    internal class BookFunction
+    {
+
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+
+        public static string GetAuthors(Book B)
+        {
+            return "    ";
+        }
+
+        public static string GetISBN(Book B)
+        {
+            return B.ISBN;
+        }
+
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString("C");
+        }
+
+        public static string GetPublicationDate(Book B)
+        {
+            return B.PublicationData.ToShortDateString();
+        }
+    }
+}
