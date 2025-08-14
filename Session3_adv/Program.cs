78c994e4a3b10dcfbc4bc767297bdcd6687d5e56
Yousef754 <yosefesam987@gmail.com>
2025-08-14 05:27:34 +0300

    V5

--- Session3_adv/Program.cs
+++ Session3_adv/Program.cs
@@ -1,25 +1,65 @@
-﻿namespace Session3_adv
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Session3_adv
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            #region v1-5
 
-            int[] numbers = { 5, 2, 9, 4, 7, 0, 1, 10 };
-            BubbleSort.PrintArrays(numbers);
-            Console.WriteLine();
+            //int[] numbers = { 5, 2, 9, 4, 7, 0, 1, 10 };
+            //BubbleSort.PrintArrays(numbers);
+            //Console.WriteLine();
 
-            //SortDelegate X=ExpressionSort.GreaterThan;
-            //BubbleSort.sortnumbers(numbers,X);
-            BubbleSort.PrintArrays(numbers);
-            Console.WriteLine();
-            SortDelegate<int> X= ExpressionSort.LessThan;
+            ////SortDelegate X=ExpressionSort.GreaterThan;
+            ////BubbleSort.sortnumbers(numbers,X);
+            //BubbleSort.PrintArrays(numbers);
+            //Console.WriteLine();
+            //SortDelegate<int> X = ExpressionSort.LessThan;
 
-            BubbleSort.sortnumbers(numbers,X);
-            BubbleSort.PrintArrays(numbers);
+            //BubbleSort.sortnumbers(numbers, X);
+            //BubbleSort.PrintArrays(numbers);
+
+            #endregion
+            #region v5
+            //PublicKey static list<int> findoddnumbers(List<int>list)
+            //    if(List>.count>0)
+            //    {for(int i=0;i<list.count;i++)
+            //      {
+            //        if(list[i]%2!=0)
+            //          result.add(list[i]);
+
+            //
+            //
+            //      return result
+            //      }
+            //    }
 
 
+
+
+            //PublicKey static list<int> findevemnumbers(List<int>list)
+            //    if(List>.count>0)
+            //    {for(int i=0;i<list.count;i++)
+            //      {
+            //        if(list[i]%2==0)
+            //          result.add(list[i]);
+
+            //
+            //
+            //      return result
+            //      }
+            //    }
+
+
+
+            // list<int>numbers=Enumerable.Range(1,100).ToList():
+            // list<int> oddnumbers=findoddnumbers(numbers);
+            // list<int> evennumbers=findevennumbers(numbers);
+            //print(oddnumbers)
+            #endregion
         }
     }
 }
